9e79d5d8c906292ae8ad0f1d94c705eb488c5f3e
gassio <gassiogi@gmail.com>
2018-01-16 16:56:42 +0200

    Created logic js file

---
+++ Frontend/9_timeline/logic.js
@@ -0,0 +1,6 @@
+// Here will be the data stored
+var startActions = [
+    {"actionID": 1 , "name": "Ενεργεια1", "actionCost": 56},
+    {"actionID": 2 , "name": "Ενεργεια2", "actionCost": 54},
+    {"actionID": 3 , "name": "Ενεργεια3", "actionCost": 7}
+]
