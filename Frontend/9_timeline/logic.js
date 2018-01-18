468df3c45d2036245d9482053df3778b3c5de9dd
MargaritaSar <34173213+MargaritaSar@users.noreply.github.com>
2018-01-18 20:50:15 +0200

    Add files via upload

--- Frontend/9_timeline/logic.js
+++ Frontend/9_timeline/logic.js
@@ -1,6 +1,8 @@
-// Here will be the data stored
 var startActions = [
     {"actionID": 1 , "name": "Ενεργεια1", "actionCost": 56},
     {"actionID": 2 , "name": "Ενεργεια2", "actionCost": 54},
     {"actionID": 3 , "name": "Ενεργεια3", "actionCost": 7}
 ]
+
+var gantt = new Gantt("#gantt", startActions) 
+
