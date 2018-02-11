6d976eb431128c448e3db65d516306620da0384a
Orestis Alagialoglou <orestisal97@gmail.com>
2018-02-11 20:40:38 +0200

    final skeleton

--- Frontend/middle_team/app/src/components/Skeleton.vue
+++ Frontend/middle_team/app/src/components/Skeleton.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="skeleton">
-
+  <body>
     <header class="head">
         <div class="head__logo">
-            <!--<img src="../assets/logo.png" alt="Logo">-->
+            <img src="../assets/Logo.png" alt="Logo">
         </div>
         <div class="head__export">
             <i class="fa fa-cloud-download fa-2x" aria-hidden="true"></i>
             <button @click="exportFunction()">Export</button>
         </div>
-        <img class="head__avatar" src="../assets/elvis.jpg" alt="Avatar">
+        <img class="head__avatar" src="../assets/avatar.png" alt="Avatar">
     </header>
 
     <div class="main_container">
         <nav>
             <div class="sections-menu">
-                <ul>
-                    <li>
-                      <a href="">ΕΠΙΧΕΙΡΗΜΑΤΙΚΟ ΜΟΝΤΕΛΟ</a>
+                <ul class="sections-menu-ul">
+                    <li class="has-sub">
+                        <a href="">ΕΠΙΧΕΙΡΗΜΑΤΙΚΟ ΜΟΝΤΕΛΟ<span class="sub-arrow"></span></a>
                       <ul>
                         <router-link to="/identity" tag="li" class="">
                           <a>ΤΑΥΤΟΤΗΤΑ</a>
@@ -27,8 +27,8 @@
                         </router-link>
                       </ul>
                     </li>
-                    <li>
-                      <a href="">ΑΝΘΡΩΠΙΝΟ ΔΥΝΑΜΙΚΟ</a>
+                    <li class="has-sub">
+                        <a href="">ΑΝΘΡΩΠΙΝΟ ΔΥΝΑΜΙΚΟ<span class="sub-arrow"></span></a>
                       <ul>
                         <router-link to="/managers" tag="li" class="">
                           <a>ΟΜΑΔΑ ΔΙΟΙΚΗΣΗΣ</a>
@@ -41,8 +41,8 @@
                         </router-link>
                       </ul>
                     </li>
-                    <li>
-                      <a href="">ΕΓΚΑΣΤΑΣΕΙΣ - ΕΞΟΠΛΙΣΜΟΣ</a>
+                    <li class="has-sub">
+                        <a href="">ΕΓΚΑΤΑΣΤΑΣΕΙΣ - ΕΞΟΠΛΙΣΜΟΣ<span class="sub-arrow"></span></a>
                       <ul>
                         <router-link to="/faculties" tag="li" class="">
                           <a>ΤΟΠΟΣ ΕΓΚΑΤΑΣΤΑΣΗΣ</a>
@@ -55,8 +55,8 @@
                         </router-link>
                       </ul>
                     </li>
-                    <li>
-                      <a href="">ΠΡΟΪΌΝΤΑ</a>
+                    <li class="has-sub">
+                        <a href="">ΠΡΟΪΟΝΤΑ - ΥΠΗΡΕΣΙΕΣ<span class="sub-arrow"></span></a>
                       <ul>
                         <router-link to="/characteristics" tag="li" class="">
                           <a>ΧΑΡΑΚΤΗΡΙΣΤΙΚΑ</a>
@@ -69,8 +69,8 @@
                         </router-link>
                       </ul>
                     </li>
-                    <li>
-                      <a href="">ΑΝΑΛΥΣΗ ΑΓΟΡΑΣ</a>
+                    <li class="has-sub">
+                        <a href="">ΑΝΑΛΥΣΗ ΑΓΟΡΑΣ<span class="sub-arrow"></span></a>
                       <ul>
                         <router-link to="/swot" tag="li" class="">
                           <a>ΑΝΑΛΥΣΗ SWOT</a>
@@ -83,8 +83,8 @@
                         </router-link>
                       </ul>
                     </li>
-                    <li>
-                      <a href="">ΣΤΡΑΤΗΓΙΚΗ MARKETING</a>
+                    <li class="has-sub">
+                        <a href="">ΣΤΡΑΤΗΓΙΚΗ MARKETING<span class="sub-arrow"></span></a>
                       <ul>
                         <router-link to="/strategy" tag="li" class="">
                           <a>ΣΤΡΑΤΗΓΙΚΗ</a>
@@ -94,8 +94,8 @@
                         </router-link>
                       </ul>
                     </li>
-                    <li>
-                      <a href="">ΧΡΗΜΑΤΟΟΙΚΟΜΙΚΟΣ ΣΧΕΔΙΑΣΜΟΣ</a>
+                    <li class="has-sub">
+                        <a href="">ΧΡΗΜΑΤΟΙΚΟΝΟΜΙΚΟΣ ΣΧΕΔΙΑΣΜΟΣ<span class="sub-arrow"></span></a>
                       <ul>
                         <router-link to="/start-actions" tag="li" class="">
                           <a>ΚΟΣΤΟΣ ΕΝΑΡΞΗΣ</a>
@@ -130,22 +130,28 @@
         </span>
         <button class="foot__next">ΜΠΡΟΣΤΑ</button>
     </footer>
-    
+      </body>
   </div>
 </template>
 
 <script>
-
 import pdfMake from "pdfmake/build/pdfmake"
 import pdfFonts from "pdfmake/build/vfs_fonts"
 pdfMake.vfs = pdfFonts.pdfMake.vfs
-
 export default {
  	name: 'Skeleton',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+    mounted(){
+              $(document).ready(function(e) {
+            $('.has-sub').click(function(){
+                event.preventDefault();
+                $(this).toggleClass('tap');
+            });
+        });
   },
   methods: {
   	// This function returns a parsed date from the db form(YYYYMMDD) into proper form with slashes(DD/MM/YYYY)
@@ -175,7 +181,6 @@ export default {
 					"Τύπος επιχείρησης: "      + db.identity[0].OrderOfBusiness
 					
 				], // Content array end
-
 				styles: {
 				    sectionHeader: {
 				      bold: true, underline: true, fontSize: 20, alignment: "left", decoration:"underline"
@@ -184,7 +189,6 @@ export default {
 				      bold: true, underline: true, fontSize: 15, alignment: "left", decoration:"underline"
 				    }
 				  }
-
 			} // docDefinition end
 			
 			// Download the PDF, named after the business name given in section 1.1
@@ -195,160 +199,124 @@ export default {
 </script>
 
 <style>
-        /* APP */
+/* APP */
+body {
+    display: flex;
+    flex-direction: column;
+    justify-content: initial;
+    align-items: stretch;
+    min-height: 100vh;
+}
 
-        .skeleton {
-            display: flex;
-            flex-direction: column;
-            justify-content: initial;
-            align-items: stretch;
-            min-height: 100vh;
-        }
+/* HEADER */
+.head {
+    background-color: rgb(30, 33, 51);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    ;
+}
+.head__logo {
+    margin-left: 20px;
+}
+.head__export {
+    color: #fff;
+    display: flex;
+    margin-right: 100px;
+    align-items: center;
+}
+.head__avatar {
+    height: 70px;
+    width: 70px;
+    border-radius: 50%;
+    margin-right: 20px;
+}
+.head__export p {
+    margin-left: 5px;
+    font-size: 20px;
+}
+.head__export i {}
+/* FOOTER */
+footer {
+    background-color: rgb(203, 202, 202);
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.foot__back {
+    margin-right: 30px;
+    background-color: rgb(41, 152, 88);
+    color: white;
+    width: 100px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-style: none;
+}
+.foot__next {
+    margin-left: 30px;
+    background-color: rgb(41, 152, 88);
+    color: white;
+    width: 100px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-style: none;
+}
+.foot__pagination {
+    font-size: 20px;
+    font-weight: 600;
+    color: black;
+}
+main {
+    flex-grow: 1;
+    overflow-y: scroll;
+}
+.main_container {
+    flex-grow: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: stretch;
+}
+nav {
+    background-color: rgb(30, 33, 51);
+    width: 350px;
+}
 
-
-        /* HEADER */
-
-        .head {
-            background-color: rgb(30, 33, 51);
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            height: 80px;
-            ;
-        }
-
-        .head__logo {
-            margin-left: 20px;
-        }
-
-        .head__export {
-            color: #fff;
-            display: flex;
-            margin-right: 100px;
-            align-items: center;
-        }
-
-
-        .head__avatar {
-            height: 70px;
-            width: 70px;
-            border-radius: 50%;
-            margin-right: 20px;
-        }
-
-        .head__export p {
-            margin-left: 5px;
-            font-size: 20px;
-        }
-
-        .head__export i {}
-
-
-        /* FOOTER */
-
-        footer {
-            background-color: rgb(203, 202, 202);
-            height: 60px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .foot__back {
-            margin-right: 30px;
-            background-color: rgb(41, 152, 88);
-            color: white;
-            width: 100px;
-            height: 40px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border-style: none;
-        }
-
-        .foot__next {
-            margin-left: 30px;
-            background-color: rgb(41, 152, 88);
-            color: white;
-            width: 100px;
-            height: 40px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border-style: none;
-        }
-
-        .foot__pagination {
-            font-size: 20px;
-            font-weight: 600;
-            color: black;
-        }
-
-
-        main {
-            flex-grow: 1;
-        }
-
-        .main_container {
-            flex-grow: 1;
-            display: flex;
-            justify-content: space-between;
-            align-items: stretch;
-        }
-
-        nav {
-            background-color: rgb(30, 33, 51);
-            width: 350px;
-        }
-
-
-        /* SECTIONS MENU */
-
-        .sections-menu {}
-
-        .sections-menu ul {
-            list-style: none;
-            margin: 0;
-        }
-
-        .sections-menu ul li {
-            padding: 15px;
-            position: relative;
-            /* width: 350px; */
-            border-top: 1px solid black;
-            background-color: rgb(30, 33, 51);
-        }
-
-        .sections-menu ul li {
-            border-right: 5px solid rgb(61, 65, 90);
-        }
-
-        .sections-menu ul ul {
-            opacity: 0;
-            position: absolute;
-            visibility: hidden;
-            left: 100%;
-            top: -2%;
-            background-color: rgb(40, 44, 63);
-        }
-
-        .sections-menu ul ul li {
-            background-color: rgb(79, 84, 115);
-            width: 250px;
-        }
-
-        .sections-menu ul li:hover ul {
-            opacity: 1;
-            visibility: visible;
-        }
-
-        .sections-menu ul li a {
-            color: white;
-        }
-
-
+/* SECTIONS MENU */
+.sections-menu-ul{
+list-style-type: none;
+padding-left:0px;
+}
+.sections-menu-ul ul{
+display: none;
+list-style-type: none;
+background-color: rgb(40, 44, 63);
+padding-left:10px;
+}
+.sections-menu-ul li.tap ul{
+display: block;
+}
+.sections-menu a{
+    padding: 10px 0 10px 20px;
+    display: block;
+    color:rgb(202, 202, 203);
+    padding-left:15px;
+    text-decoration:none;
+}
+.sections-menu .sub-arrow:after{
+    content: '\203A';
+    float:right;
+    margin-right: 20px;
+    transform: rotate(90deg);
+    -webkit-transform: rotate(90deg);
+    -moz-transform: rotate(90deg);
+}
         /* BASIC ELEMENTS */
-
         .basic_button {
             background-color: rgb(24, 146, 105);
             width: 100px;
