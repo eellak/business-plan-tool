3feb1db72461efb4952b5674652ec42d1e24e9f8
Kostas Tsampazis <tsampazk@csd.auth.gr>
2018-02-14 13:16:14 +0200

    Added some new vuex actions, mutations, getters
    actions, mutations, getters for:
    - Identity
    - Clients
    - Description
    - Employees
    - Completed Partners

--- Frontend/middle_team/app/src/store.js
+++ Frontend/middle_team/app/src/store.js
@@ -9,45 +9,18 @@ const serverUrl = 'http://play-trinity.com/theo/bplantool/api'
 export const store = new Vuex.Store({
 	state: {
 			//Team 2.1 
-			identity: [{
-				"Id": 1,
-				"BusinessPlanId": 1,
-				"Name": "ΑΠΘ",
-				"Date": 20180108,
-				"LegalForm": "Α.Ε.",
-				"OrderOfBusiness": "Πανεπιστημιο",
-			}],
+			identity: [],
 			
-			clients: [
-				{"ID": 1, "IdentityId": 1, "Name": "client 1"},
-				{"ID": 2, "IdentityId": 1, "Name": "client 2"},
-				{"ID": 3, "IdentityId": 1, "Name": "client 3"}
-			],
+			clients: [],
 			
 			//Team 2.2
-			description: [{
-					"ID": 1,
-					"IdentityId": 1,
-					"Text": "Μεγαλο κειμενο για την περιγραφη της επιχειρησης. Lorem ipsum dolor sit amet, \
-							consectetur adipiscing elit. Sed gravida eget diam nec rhoncus. Phasellus luctus \
-							aliquam arcu, eget finibus tellus blandit eget. Nunc porta orci dolor, eu placerat \
-							quam consequat ut. Sed nibh mauris, dictum et feugiat vitae, facilisis elementum \
-							leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent \
-							taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \
-							Maecenas sodales scelerisque tristique."
-			}],
+			description: [],
 			
 			//Team 3.1
 			managers: [],
 
 			//Team 3.2
-			employees: [
-				{"ID": 1, "BusinessPlanId": 1, "Job": "job 3"},
-				{"ID": 2, "BusinessPlanId": 1, "Job": "job 3"},
-				{"ID": 3, "BusinessPlanId": 1, "Job": "job 4"},
-				{"ID": 4, "BusinessPlanId": 1, "Job": "job 5"},
-				{"ID": 5, "BusinessPlanId": 1, "Job": "job 5"}
-			],
+			employees: [],
 
 			//Team 3.3
 			partners: [],	
@@ -185,7 +158,107 @@ export const store = new Vuex.Store({
 	},
 
 	actions: {
-		// Human resources 3.1
+		// Identity 2.1,  Categories based on FinalJsons.js
+		getIdentities: function ({ commit }) {
+			axios.get(serverUrl + "/identity")
+				.then(function (response)
+				{
+					commit('GET_IDENTITIES', response.data)
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		},
+		createIdentity: function ({ commit }, payload) {
+			axios.post(serverUrl + "/identity", payload)
+			.then(function (response)
+			{
+				console.log("payload of create: ", payload)
+				commit('CREATE_IDENTITY', payload)
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		},	
+		deleteIdentity: function ({ commit }, payload) {
+			axios.delete(serverUrl + "/identity/" + payload)
+			.then(function (response)
+			{
+				commit('DELETE_IDENTITY', payload)
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+		},
+		
+		// Clients 2.1
+		getClients: function ({ commit }) {
+			axios.get(serverUrl + "/client")
+				.then(function (response)
+				{
+					commit('GET_CLIENTS', response.data)
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		},
+		createClient: function ({ commit }, payload) {
+			axios.post(serverUrl + "/client", payload)
+			.then(function (response)
+			{
+				console.log("payload of create: ", payload)
+				commit('CREATE_CLIENT', payload)
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		},	
+		deleteClient: function ({ commit }, payload) {
+			axios.delete(serverUrl + "/client/" + payload)
+			.then(function (response)
+			{
+				commit('DELETE_CLIENT', payload)
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+		},
+
+		// Description 2.2
+		getDescriptions: function ({ commit }) {
+			axios.get(serverUrl + "/description")
+				.then(function (response)
+				{
+					commit('GET_DESCRIPTIONS', response.data)
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		},
+		createDescription: function ({ commit }, payload) {
+			axios.post(serverUrl + "/description", payload)
+			.then(function (response)
+			{
+				console.log("payload of create: ", payload)
+				commit('CREATE_DESCRIPTION', payload)
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		},	
+		deleteDescription: function ({ commit }, payload) {
+			axios.delete(serverUrl + "/description/" + payload)
+			.then(function (response)
+			{
+				commit('DELETE_DESCRIPTION', payload)
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+		},
+
+
+		// Managers 3.1
 		getManagers: function ({ commit }) {
 			axios.get(serverUrl + "/manager")
 				.then(function (response)
@@ -200,33 +273,67 @@ export const store = new Vuex.Store({
 			axios.post(serverUrl + "/manager", payload)
 			.then(function (response)
 			{
-					console.log("payload of create: ", payload)
-					commit('CREATE_MANAGER', payload)
+				console.log("payload of create: ", payload)
+				commit('CREATE_MANAGER', payload)
 				})
 				.catch(function (err) {
 					console.log(err)
 				})
-			},
-			deleteManager: function ({ commit }, payload) {
-				axios.delete(serverUrl + "/manager/" + payload)
+		},	
+		deleteManager: function ({ commit }, payload) {
+			axios.delete(serverUrl + "/manager/" + payload)
+			.then(function (response)
+			{
+				commit('DELETE_MANAGER', payload)
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+		},
+
+		// Employees 3.2
+		getEmployees: function ({ commit }) {
+			axios.get(serverUrl + "/employee")
 				.then(function (response)
 				{
-					commit('DELETE_MANAGER', payload)
+					commit('GET_EMPLOYEES', response.data)
 				})
 				.catch(function (err) {
 					console.log(err)
 				})
-			},
-			// Human resources 3.2
-			getPartners: function ({ commit }) {
-				axios.get(serverUrl + "/partner")
-				.then(function (response)
-				{
-					commit('GET_PARTNERS', response.data)
+		},
+		createEmployee: function ({ commit }, payload) {
+			axios.post(serverUrl + "/employee", payload)
+			.then(function (response)
+			{
+				console.log("payload of create: ", payload)
+				commit('CREATE_EMPLOYEE', payload)
 				})
 				.catch(function (err) {
 					console.log(err)
 				})
+		},	
+		deleteEmployee: function ({ commit }, payload) {
+			axios.delete(serverUrl + "/employee/" + payload)
+			.then(function (response)
+			{
+				commit('DELETE_EMPLOYEE', payload)
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+		},
+
+		// Partners 3.3	
+		getPartners: function ({ commit }) {
+			axios.get(serverUrl + "/partner")
+			.then(function (response)
+			{
+				commit('GET_PARTNERS', response.data)
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
 		},
 		createPartner: function ({ commit }, payload) {
 			axios.post(serverUrl + "/partner", payload)
@@ -239,9 +346,101 @@ export const store = new Vuex.Store({
 					console.log(err)					
 				})
 		},
+		deletePartner: function ({ commit }, payload) {
+			axios.delete(serverUrl + "/partner/" + payload)
+			.then(function (response)
+			{
+				commit('DELETE_PARTNER', payload)
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+		},
+
+		// Employee Salaries 3.4
+
+		// Faculties 4.1
+
+		// Equipment 4.2
+
+		// RunningCost 4.3
+
+		// RunningCost - FacultyCosts 4.3
+
+		// RunningCost - EquipmentCosts 4.3
+
+		// Products 5
+
+		// SWOT 6.1
+
+		// PESTEL - Factors 6.2
+
+		// Notes 6.3
+
+		// Strategy 7.1
+
+		// MarketingActions 7.2
+
+		// StartActions 8.1
+
+		// FunctionCost 8.2
+
+		// FunctionCost - Functions 8.2
+
+		// Deadspots 8.3
+
+		// Links 9
+
+		// Conclusion 1.1
 	},
 
 	mutations:{
+		// Identity 2.1,  Categories based on FinalJsons.js
+		GET_IDENTITIES: (state, payload) => {
+			state.identity = payload
+		},		
+		CREATE_IDENTITY: (state, payload) => {
+			state.identity.push(payload)
+		},
+		DELETE_IDENTITY: (state, payload) => {
+			for (var i=0, l = state.identity.length; i < l; i++) {
+				if (state.identity[i].ID === payload) {
+					state.identity.splice(i, 1)
+				}
+			}
+		},
+		
+		// Clients 2.1
+		GET_CLIENTS: (state, payload) => {
+			state.clients = payload
+		},		
+		CREATE_CLIENT: (state, payload) => {
+			state.clients.push(payload)
+		},
+		DELETE_CLIENT: (state, payload) => {
+			for (var i=0, l = state.clients.length; i < l; i++) {
+				if (state.clients[i].ID === payload) {
+					state.clients.splice(i, 1)
+				}
+			}
+		},
+
+		// Description 2.2
+		GET_DESCRIPTIONS: (state, payload) => {
+			state.description = payload
+		},		
+		CREATE_DESCRIPTION: (state, payload) => {
+			state.description.push(payload)
+		},
+		DELETE_DESCRIPTION: (state, payload) => {
+			for (var i=0, l = state.description.length; i < l; i++) {
+				if (state.description[i].ID === payload) {
+					state.description.splice(i, 1)
+				}
+			}
+		},
+
+		// Managers 3.1
 		GET_MANAGERS: (state, payload) => {
 			state.managers = payload
 		},		
@@ -250,31 +449,211 @@ export const store = new Vuex.Store({
 			state.managers.push(payload)
 		},
 		DELETE_MANAGER: (state, payload) => {
-			console.log("payload: ", payload)
-			console.log('state.managers: ', state.managers)
+			//console.log("payload: ", payload)
+			//console.log('state.managers: ', state.managers)
 			for (var i=0, l = state.managers.length; i < l; i++) {
 				if (state.managers[i].ID === payload) {
-					console.log("id: ", state.managers[i].ID)
+					//console.log("id: ", state.managers[i].ID)
 					state.managers.splice(i, 1)
 				}
 			}
 		},
 
+		// Employees 3.2
+		GET_EMPLOYEES: (state, payload) => {
+			state.employees = payload
+		},		
+		CREATE_EMPLOYEE: (state, payload) => {
+			state.employees.push(payload)
+		},
+		DELETE_EMPLOYEE: (state, payload) => {
+			for (var i=0, l = state.employees.length; i < l; i++) {
+				if (state.employees[i].ID === payload) {
+					state.employees.splice(i, 1)
+				}
+			}
+		},
+
+		// Partners 3.3	
 		GET_PARTNERS: (state, payload) => {
 			state.partners = payload
 		},		
 		CREATE_PARTNER: (state, payload) => {
-			state.partners.pop()
 			state.partners.push(payload)
 		},
+		DELETE_PARTNER: (state, payload) => {
+			for (var i=0, l = state.partners.length; i < l; i++) {
+				if (state.partners[i].ID === payload) {
+					state.partners.splice(i, 1)
+				}
+			}
+		},
+
+		// Employee Salaries 3.4
+
+		// Faculties 4.1
+
+		// Equipment 4.2
+
+		// RunningCost 4.3
+
+		// RunningCost - FacultyCosts 4.3
+
+		// RunningCost - EquipmentCosts 4.3
+
+		// Products 5
+
+		// SWOT 6.1
+
+		// PESTEL - Factors 6.2
+
+		// Notes 6.3
+
+		// Strategy 7.1
+
+		// MarketingActions 7.2
+
+		// StartActions 8.1
+
+		// FunctionCost 8.2
+
+		// FunctionCost - Functions 8.2
+
+		// Deadspots 8.3
+
+		// Links 9
+
+		// Conclusion 1.1
 	},
 
 	getters:{
+		// All the getters getting the first (0th) resource, assume that there exists
+		// one of that resource inside the state manager (meaning one per business plan)
+
+		// Identity 2.1,  Categories based on FinalJsons.js
+		identity: state => {
+			return state.identity[0] // Returns the first (0th) identity
+		},
+
+		// Clients 2.1
+		clients: state => {
+			return state.clients
+		},
+
+		// Description 2.2
+		description: state => {
+			return state.description[0] // Returns the first (0th) description
+		},
+
+		// Managers 3.1
 		managers: state => {
 			return state.managers
 		},
+
+		// Employees 3.2
+		employees: state => {
+			return state.employees
+		},
+
+		// Partners 3.3	
 		partners: state => {
 			return state.partners
+		},
+
+		// Employee Salaries 3.4
+		employeeSalaries: state => {
+			return state.employeeSalaries
+		},
+
+		// Faculties 4.1
+		faculties: state => {
+			return state.faculties
+		},
+
+		// Equipment 4.2
+		equipment: state => {
+			return state.equipment
+		},
+
+		// RunningCost 4.3
+		// Returns the full runningCost resource including faculty and equipment costs
+		runningCost: state => {
+			return state.runningCost[0] // Returns the first (0th) runningCost
+		},
+
+		// RunningCost - FacultyCosts 4.3
+		// Returns only the facultyCosts array from inside the first (0th) runningCost
+		facultyCosts: state => {
+			return state.runningCost[0].FacultyCosts 
+		},
+
+		// RunningCost - EquipmentCosts 4.3
+		// Returns only the equipmentCosts array from inside the first (0th) runningCost		
+		equipmentCosts: state => {
+			return state.runningCost[0].EquipmentCosts
+		},
+
+		// Products 5
+		products: state => {
+			return state.products
+		},
+
+		// SWOT 6.1
+		swot: state => {
+			return state.swot[0] // Returns the first (0th) swot
+		},
+
+		// PESTEL - Factors 6.2
+		factors: state => {
+			return state.factors
+		},
+
+		// Note 6.3
+		note: state => {
+			return state.note[0] // Returns the first (0th) note
+		},
+
+		// Strategy 7.1
+		strategy: state => {
+			return state.strategy[0] // Returns the first (0th) strategy
+		},
+
+		// MarketingActions 7.2
+		marketingActions: state => {
+			return state.marketingActions
+		},
+
+		// StartActions 8.1
+		startActions: state => {
+			return state.startActions
+		},
+
+		// FunctionCost 8.2
+		// Returns the full functionCost resource including the individual functions
+		// and the totalCost
+		functionCost: state => {
+			return state.functionCost[0] // Returns the first (0th) functionCost
+		},
+
+		// FunctionCost - Functions 8.2
+		// Returns only the functions array from inside the first (0th) functionCost resource
+		functions: state => {
+			return state.functionCost[0].Functions
+		},
+
+		// Deadspots 8.3
+		deadspots: state => {
+			return state.deadspots
+		},
+
+		// Links 9
+		links: state => {
+			return state.links
+		},
+
+		// Conclusion 1.1
+		conclusion: state => {
+			return state.conclusion[0] // Returns the first (0th) conclusion
 		},
 	}
 })
