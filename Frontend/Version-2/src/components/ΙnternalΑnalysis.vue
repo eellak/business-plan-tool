8fa1b943eb1c945eede97532fddd05011c595771
gassio <gassiogi@gmail.com>
2018-05-16 23:28:15 +0300

    Internal analysis: longtext component (in progress)

--- Frontend/Version-2/src/components/ΙnternalΑnalysis.vue
+++ Frontend/Version-2/src/components/ΙnternalΑnalysis.vue
@@ -1,37 +1,33 @@
 <template>
-			<div class="main__body">
+	<div class="main__body" style="margin-left:40px;margin-right:40px;">
 
 			<div class="main__header">
 				<h1>Εσωτερική Ανάλυση</h1>
 			</div>
 
+			<div class="longtext">
+				<h3 class="longtext__title">Περιγραφή της επιχείρησης</h3>
+				<p class="longtext__content">Μπλα μπλα μπλα</p>
+				<span class="longtext__addarea">
+					<a class="longtext__addButton" @click="openTextarea($event)">+Συμπληρώστε</a>
+					<el-input class="longtext__textarea" type="textarea"></el-input>	
+				</span>	
+			</div>
 
-
-
-			<div class="group">
+			<!-- <div class="group">
 				<div class="title_of_group">Μέτοχοι της επιχείρησης</div>
                 <div id="input" style="flex-grow:1;margin-right:40px;margin-top:10px;">
 					<el-input class="group-input" placeholder="Βαλαωρίτου 15" v-model="shareholder"></el-input>
 				</div>
                 <button class="red_inside_information" @click="addShareholder()">+Προσθέστε μέτοχο</button>
-			</div>
-			
+			</div> -->
 
-
-
-
-		<div class="main__footer">
-			
-				
+			<div class="main__footer">
 				<div class="footer__buttons">
-				<!-- <button class="button">BACK</button> -->
-				<!-- <button class="button">NEXT</button> -->
 				</div>
 			</div>
 
-
-			</div>
-			
+	</div>	
 </template>
 <script>
 export default {
@@ -51,7 +47,10 @@ export default {
 				  
 	},
 	methods: {
-		// Here you can write your functions.
+		openTextarea(ev) {
+			ev.currentTarget.style.display = 'none'
+			ev.currentTarget.parentElement.getElementsByClassName('longtext__textarea')[0].style.display = 'block'
+		},
 		addShareholder() { 
 			var shareholderInput = document.getElementsByClassName("group-input")[0];
 
@@ -64,130 +63,55 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.button{
-	background-color: #299858;
-    color: #fff;
-    border: #299858;
-    padding: 15px;
-    text-align: center;
-    font-size: 20px;
-    width: 160px;
-	border-radius:5px;
+<style>
+
+/* ==============================================
+                #LONGTEXT
+================================================= */
+
+.longtext {
+	border-radius: 3px;
+	padding: 10px 15px;
 }
 
-.instructions_button{
-    float: right;
+.longtext:hover {
+	border: 1px dashed #47A2D3;
+	background-color: #ECF6FB;
 }
 
-.main__container{
-	display:flex;
-	flex-grow: 1;
-	overflow-y:auto;
-}
+	.longtext__title {
+		font-weight: 300;
+		margin: 0;
+	}
 
-.main__header{
-	margin-right:20px;
-	margin-left:20px;
-	text-align:center;
-}
+	.longtext__content {
+		display: none;
+	}
 
-.main__body{
-	margin-top:20px;
-	margin-bottom:20px;
-	display:flex;
-	flex-direction: column;
-	flex-grow:1;
-	overflow-y:auto;	
-}
+	.longtext__addarea {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		cursor: pointer;
+	}
 
-.main__body_header{
-	text-size:20px;
-}
+		.longtext__addButton {
+			color: #EB5757;
+			align-self: center;
+		}
 
-.main__footer{
-	margin-top:20px;
-	margin-right:20px;
-    margin-left:20px;	
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column-reverse;
-}
-
-.group{
-	margin-top:10px;
-	margin-bottom:10px;
-	display:flex;
-
-    flex-direction: column;
-	margin-left:20px;
-	margin-right:20px;
-}
-
-.input_group:hover { 
-	padding: 10px 10px 10px 10px;
-    background-color: rgb(238,238,238);
-	border-radius:10px;
-	
-}
-
-.title{
-	font-size:18px;
-	text-align: center;
-	margin-right:50px;
-	margin-left:20px;
-	width:300px;
-	display:flex;
-	justify-content:flex-end;
-}
-
-.steps{
-	border-style: solid;
-    border-width: 0px;
-	padding:20px 20px 20px 20px;
-	border-radius:10px;
-	border-color:gray;
-	margin:20px 20px 20px 20px;
-}
-
-.footer__buttons{
-	display:flex;
-	justify-content:space-between;
-	margin:20px 20px 20px 20px;
-}
-
-.right_column{
-    width:300px;
-    background-color: rgb(192, 192, 192);
-}
-
-.instructions_title{
-	font-size:24px;
-	border-bottom: 2px solid rgb(220,220,220);
-	padding-bottom:10px;
-	padding-top:10px;
-	padding-left:10px;
-	font-color:rgb(30, 33, 51);
-}
-
-.instructions_body{
-	padding:15px 15px 15px 15px;
-	font-color:rgb(30, 33, 51);
-	font-size:20px;
-}
-
-.save_big_button{
-    bottom:0;
-    position:fixed;
-    width:0;
-}
+		.longtext__textarea {
+			display: none;
+			transition: 0.6s;
+		}
 
 
 
 
 
-
-
+/* ==============================================
+                #SHAREHOLDERS
+================================================= */
 
 .title_of_group{
     margin-left:35px;
