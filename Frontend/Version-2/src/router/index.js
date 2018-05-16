8a35bb3152f14e717e9f8f8f6453f663829705bb
Orestis Alagialoglou <orestisal97@gmail.com>
2018-05-16 21:18:35 +0300

    Update index.js

--- Frontend/Version-2/src/router/index.js
+++ Frontend/Version-2/src/router/index.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import BasicInformation from '@/components/BasicInformation'
+import InternalAnalysis from '@/components/ΙnternalΑnalysis'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/basic-information', name: 'BasicInformation', component: BasicInformation },
+    { path: '/internal-analysis', name: 'ΙnternalΑnalysis', component: InternalAnalysis },
   ],
   mode: 'history'
 })
